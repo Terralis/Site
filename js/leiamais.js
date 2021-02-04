82d5a709c8fe8ef17c51bfbd050b53159c300981
pamscarvalho <71795901+pamscarvalho@users.noreply.github.com>
2021-02-04 09:39:15 -0300

    Ajuda
    Tela de ajuda funcionado, falta só preencher com as respostas.

---
+++ js/leiamais.js
@@ -0,0 +1,80 @@
+function leiaMais1(){
+	var pontos = document.getElementById("pontos1");
+	var maisTexto = document.getElementById("mais1");
+	var btnLeiaMais = document.getElementById("btnLeiaMais1");
+
+	if (pontos.style.display === "none") {
+		pontos.style.display = "inline";
+		maisTexto.style.display = "none";
+		btnLeiaMais.innerHTML = "Leia mais &raquo;";
+	}
+	else{
+		pontos.style.display = "none";
+		maisTexto.style.display = "inline";
+		btnLeiaMais.innerHTML = "Leia menos &raquo;";
+	}
+}
+function leiaMais2(){
+	var pontos = document.getElementById("pontos2");
+	var maisTexto = document.getElementById("mais2");
+	var btnLeiaMais = document.getElementById("btnLeiaMais2");
+
+	if (pontos.style.display === "none") {
+		pontos.style.display = "inline";
+		maisTexto.style.display = "none";
+		btnLeiaMais.innerHTML = "Leia mais &raquo;";
+	}
+	else{
+		pontos.style.display = "none";
+		maisTexto.style.display = "inline";
+		btnLeiaMais.innerHTML = "Leia menos &raquo;";
+	}
+}
+function leiaMais3(){
+	var pontos = document.getElementById("pontos3");
+	var maisTexto = document.getElementById("mais3");
+	var btnLeiaMais = document.getElementById("btnLeiaMais3");
+
+	if (pontos.style.display === "none") {
+		pontos.style.display = "inline";
+		maisTexto.style.display = "none";
+		btnLeiaMais.innerHTML = "Leia mais &raquo;";
+	}
+	else{
+		pontos.style.display = "none";
+		maisTexto.style.display = "inline";
+		btnLeiaMais.innerHTML = "Leia menos &raquo;";
+	}
+}
+function leiaMais4(){
+	var pontos = document.getElementById("pontos4");
+	var maisTexto = document.getElementById("mais4");
+	var btnLeiaMais = document.getElementById("btnLeiaMais4");
+
+	if (pontos.style.display === "none") {
+		pontos.style.display = "inline";
+		maisTexto.style.display = "none";
+		btnLeiaMais.innerHTML = "Leia mais &raquo;";
+	}
+	else{
+		pontos.style.display = "none";
+		maisTexto.style.display = "inline";
+		btnLeiaMais.innerHTML = "Leia menos &raquo;";
+	}
+}
+function leiaMais5(){
+	var pontos = document.getElementById("pontos5");
+	var maisTexto = document.getElementById("mais5");
+	var btnLeiaMais = document.getElementById("btnLeiaMais5");
+
+	if (pontos.style.display === "none") {
+		pontos.style.display = "inline";
+		maisTexto.style.display = "none";
+		btnLeiaMais.innerHTML = "Leia mais &raquo;";
+	}
+	else{
+		pontos.style.display = "none";
+		maisTexto.style.display = "inline";
+		btnLeiaMais.innerHTML = "Leia menos &raquo;";
+	}
+}
